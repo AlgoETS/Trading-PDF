b857f37d47e7c2a47c3fd6be8898d0e13259e18b
JSMC <41307978+JSMCGit@users.noreply.github.com>
2023-09-28 21:22:38 -0400

    Add files via upload

---
+++ code examples/chapter1/starterCode.py
@@ -0,0 +1,26 @@
+# Below are the import statements 
+
+
+# Below are the global variables
+
+
+# Below are the custom classes and methods 
+
+
+# Below is the TestWrapper/EWrapper class
+
+
+# Below is the TestClient/EClient Class 
+
+
+# Below is TestApp Class 
+
+
+# Below is the program execution
+
+
+# Below is the input area
+
+
+# Below is the logic processing area
+
